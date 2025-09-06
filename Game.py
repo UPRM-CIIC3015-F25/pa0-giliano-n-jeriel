a186ad4c46145ad9cfeffb54fe5ec3e18b46bb28
GiliAnoT <giliano.torres@upr.edu>
2025-09-05 20:38:10 -0400

    Completed Task 1 Completed Task 6

--- Game.py
+++ Game.py
@@ -1,5 +1,8 @@
 import pygame, sys, random
 
+pygame.mixer.init(4100, -16, 1, 512)
+paddle_hit_sound = pygame.mixer.Sound("sounde.wav")
+
 def ball_movement():
     """
     Handles the movement of the ball and collision detection with the player and screen boundaries.
@@ -23,8 +26,9 @@ def ball_movement():
         if abs(ball.bottom - player.top) < 10:  # Check if ball hits the top of the paddle
             # TODO Task 2: Fix score to increase by 1
             score = 1  # Increase player score
+            paddle_hit_sound.play(loops=0, maxtime=-1, fade_ms=2)
             ball_speed_y *= -1  # Reverse ball's vertical direction
-            # TODO Task 6: Add sound effects HERE
+
 
     # Ball collision with top boundary
     if ball.top <= 0:
@@ -75,7 +79,6 @@ bg_color = pygame.Color('grey12')
 
 # Game Rectangles (ball and player paddle)
 ball = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 30, 30)  # Ball (centered)
-# TODO Task 1 Make the paddle bigger
 player_height = 15
 player_width = 200
 player = pygame.Rect(screen_width/2 - 45, screen_height - 20, player_width, player_height)  # Player paddle

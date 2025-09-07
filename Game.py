bf7c7f0db58933eb1ffbf7d801e2ff53383fc843
GiliAnoT <giliano.torres@upr.edu>
2025-09-07 11:56:28 -0400

    Completed Task 1 Completed Task 2 Completed Task 3 Completed Task 6

--- Game.py
+++ Game.py
@@ -120,12 +120,12 @@ while True:
     player_movement()
 
     # Visuals
+    clear_grey=pygame.Color('steelblue4')
     light_grey = pygame.Color('grey83')
     red = pygame.Color('red')
     screen.fill(bg_color)  # Clear screen with background color
     pygame.draw.rect(screen, light_grey, player)  # Draw player paddle
-    # TODO Task 3: Change the Ball Color
-    pygame.draw.ellipse(screen, light_grey, ball)  # Draw ball
+    pygame.draw.ellipse(screen, clear_grey, ball)  # Draw ball
     player_text = basic_font.render(f'{score}', False, light_grey)  # Render player score
     screen.blit(player_text, (screen_width/2 - 15, 10))  # Display score on screen
 

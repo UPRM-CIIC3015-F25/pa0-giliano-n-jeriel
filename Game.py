4f9fa5d4bb47cc563331b242e2782576f96e0617
jerielrodriguez <jeriel.rodriguez3@upr.edu>
2025-09-08 10:44:37 -0400

    Task 5 Commit and Push

--- Game.py
+++ Game.py
@@ -15,7 +15,7 @@ def ball_movement():
 
     # Start the ball movement when the game begins
     # TODO Task 5 Create a Merge Conflict
-    speed = 7
+    speed = 5
     if start:
         ball_speed_x = speed * random.choice((1, -1))  # Randomize initial horizontal direction
         ball_speed_y = speed * random.choice((1, -1))  # Randomize initial vertical direction
@@ -96,7 +96,6 @@ start = False  # Indicates if the game has started
 # Main game loop
 while True:
     # Event handling
-    # TODO Task 4: Add your name
     name = "Jeriel Rodriguez"
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # Quit the game
